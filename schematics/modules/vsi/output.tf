10e857c5283c13fea69fb0f28ce705f64880cd6b
Travis CI User <travis@example.org>
2022-12-06 12:23:39 +0000

    v1.1.2 release

--- schematics/modules/vsi/output.tf
+++ schematics/modules/vsi/output.tf
@@ -3,7 +3,7 @@ output "HOSTNAME" {
 }
 
 output "PRIVATE-IP" {
-  value		= ibm_is_instance.vsi.primary_network_interface[0].primary_ipv4_address
+  value		= ibm_is_instance.vsi.primary_network_interface.0.primary_ip.0.address
 }
 
 output "VPC" {
